84f204831b6ae8cebe977abd4be63b8c95313d85
vsrm@outlook.in <vsrm@outlook.in>
2015-10-24 01:03:58 +0530

    Add traffic manager script

---
+++ FabrikamFiber.CallCenter/FabrikamFiber.Web/Deploy/ConfigAzureTrafficManager.ps1
@@ -0,0 +1,25 @@
+﻿param
+(
+	[String] $TrafficManagerProfileName,
+	[Int] $Operation
+)
+
+Write-Verbose -Verbose "Getting profile for $TrafficManagerProfileName"
+$trprofile = Get-AzureTrafficManagerProfile -Name $TrafficManagerProfileName
+Write-Verbose -Verbose $trprofile.DomainName
+Write-Verbose -Verbose "Performing Operation $Operation"
+if ($Operation -eq 0) {
+# Disable Endpoint[0]
+$trprofile.Endpoints[0].Status = "Disabled"
+} elseif ($Operation -eq 1) {
+# Enable Endpoint[0] & Disable Endpoint[1]
+$trprofile.Endpoints[0].Status = "Enabled"
+$trprofile.Endpoints[1].Status = "Disabled"
+} else {
+# Enable both Endpoints
+$trprofile.Endpoints[0].Status = "Enabled"
+$trprofile.Endpoints[1].Status = "Enabled"
+}
+Write-Verbose -Verbose "Setting profile for $TrafficManagerProfileName"
+Set-AzureTrafficManagerProfile -TrafficManagerProfile $trprofile
+
